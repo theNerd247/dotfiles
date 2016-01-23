2ccf28a026f4a09b5e5a94fbaf22014312a2fd4d
theNerd247 <noah.harvey247 gm>
2016-01-23 17:56:14 -0500

    added install scripts

---
+++ install.sh
@@ -0,0 +1,42 @@
+function getInstPath()
+{
+	echo ".$(basename $1)"
+}
+
+function lnk()
+{
+	cd $BASEPATH
+
+	if [[ -z $2 ]]; then
+		instPath="$(getInstPath $1)"
+	else
+		instPath="$(getInstPath $2)"
+	fi
+
+	ln -s -T "$HOME/.dotfiles/$1" "$instPath"
+}
+
+function inst()
+{
+	cd "$BASEPATH/$(getInstPath $1)"
+	./install.sh
+}
+
+#go to our install directory
+
+BASEPATH="$1"
+
+#create the symbolic links to the config paths
+
+lnk astylerc
+lnk fonts
+lnk oh-my-zsh 
+lnk vimrc vim
+lnk vimrc/vimrc 
+lnk xmobar/xmobarcc
+lnk xmonad
+lnk Xresources
+lnk zshrc
+
+inst vim
+inst oh-my-zsh
