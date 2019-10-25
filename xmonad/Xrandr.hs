09e526c6694d202eb2e2dcbc3733983c7839107e
theNerd247 <noah.harvey247 gm>
2019-10-24 23:50:28 -0400

    using bettter algebra and parser

--- xmonad/Xrandr.hs
+++ xmonad/Xrandr.hs
@@ -2,34 +2,54 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 
 module Main where
 
-import Prelude hiding (takeWhile, take)
-import Numeric.Natural
-import Data.Foldable (foldMap)
-import Control.Arrow
 import Control.Applicative
-import Data.Attoparsec.Text
-import Data.Monoid
-import Data.List (partition, find, sortOn)
-import Data.Functor.Foldable
-import Shelly (shelly, run, run_)
-import Data.Maybe (listToMaybe)
-import qualified Data.Text as T
+import Control.Arrow
 import Control.Monad ((>=>))
+import Data.Attoparsec.Text
 import Data.Bifunctor (bimap)
+import Data.Foldable (foldMap)
+import Data.Functor.Foldable
+import Data.List (partition, find, sortOn)
+import Data.Maybe (listToMaybe)
+import Data.Monoid
+import Data.String
+import Numeric.Natural
+import Prelude hiding (takeWhile, take)
+import Shelly (shelly, run, run_)
+import qualified Data.Text as T
+
+-- | Requirements
+--
+--  * Screens can be either: Connected or Disconnected
+--  * Connected Screens can be Enabled or Disabled
+--  * Enabled screens must have a Mode and Rotation defined
+--  * Default rotation is Normal which means the screen is upright).
+--  * Only 1 screen can be the Primary, and all the others must be Secondary
+--  * Disabled / Disconnected screens must not have a position
+--  * Connected Screens must have a position relative to another screen
+--      Secondary -> Primary | Secondary; Primary doesn't have a position
+--
+--  * Disabled and Disconnected screens should coorespond to the command
+--    `--output <OutputName> --off`
+--  * We would like to keep the Disabled information for later. When the screen
+--    state expression is used in a State monad this will allow for disabling
+--    secondary screens that are currently enabled.
+--  * 
 
 -- | represents the physical layout of those screens which are connected
-data ScreensF b = 
-    NoScreens
-  | Disabled OutputName b
+data ScreenF b = 
+    Primary      OutputName Config
+  | Secondary    OutputName Config Position b
+  | Disabled     OutputName Config b
   | Disconnected OutputName b
-  | Primary OutputName Config b
-  | Secondary OutputName Position Config b
   deriving (Show, Functor)
 
-type Screens = Fix ScreensF
+type Screens = Fix ScreenF
 
 data Position =
     RightOf
@@ -39,14 +59,16 @@ data Position =
   | SameAs 
   deriving (Show)
 
-type OutputName = T.Text
+newtype OutputName = OutputName { name :: T.Text } 
+  deriving (Show, Eq, Ord, IsString)
 
 data Config = Config
   { rotation :: Rotation
   , mode     :: Mode
   } deriving (Show, Eq, Ord)
 
-type Mode = (Natural, Natural)
+newtype Mode = Mode { modeName :: (Natural, Natural) }
+  deriving (Show, Eq, Ord)
 
 data Rotation =
     Normal
@@ -60,21 +82,8 @@ type Cmd = [T.Text]
 class ToCmd a where
   buildCmd :: a -> Cmd
 
-instance (ToCmd a) => ToCmd (Maybe a) where
-  buildCmd = maybe [] buildCmd
-
-instance (Show a, Show b) => ToCmd (a, b) where
-  buildCmd (x,y) =
-    [ "--mode"
-    , (T.pack . show $ x) <> "x" <> (T.pack . show $ y)
-    ]
-
-instance ToCmd Output where
-  buildCmd Output{ name, status } =
-    [ "--output"
-    , name 
-    ] 
-    <> (buildCmd status)
+instance ToCmd OutputName where
+  buildCmd n = ["--output", name n]
 
 instance ToCmd Config where
   buildCmd m = 
@@ -83,22 +92,30 @@ instance ToCmd Config where
     
 instance ToCmd Rotation where
   buildCmd x = ["--rotate", rotateOption x]
+    where
+      rotateOption Normal      = "normal"
+      rotateOption RotateLeft  = "left"
+      rotateOption RotateRight = "right"
+      rotateOption Inverted    = "inverted"
 
-rotateOption :: Rotation -> T.Text
-rotateOption Normal      = "normal"
-rotateOption RotateLeft  = "left"
-rotateOption RotateRight = "right"
-rotateOption Inverted    = "inverted"
+instance ToCmd Mode where
+  buildCmd m =
+    [ "--mode"
+    , (T.pack . show . modeX $ m) <> "x" <> (T.pack . show . modeY $ m)
+    ]
 
-buildCmd' :: (ToCmd a, HasOutput a) => ScreensF (Screens, Cmd) -> Cmd
-buildCmd' NoScreens                      = []
-buildCmd' Disconnected n (_, cmds)       = cmds <> (buildCmd n) <> ["--off"]
-buildCmd' Disabled n     (_, cmds)       = cmds <> (buildCmd n) <> ["--off"]
-buildCmd' (Primary o c   (_, cmds))      = cmds <> (buildCmd o) <> (buildCmd c) <> ["--primary"]
-buildCmd' (Secondary o p (Fix os, cmds)) = cmds <> (buildCmd o) <> (buildCmd p) <> [positionArg p, output os]
+modeX = fst . modeName
 
-makeCmd :: (ToCmd a, HasOutput a) => Screens -> Cmd
+modeY = snd . modeName
+
+makeCmd :: Screens -> Cmd
 makeCmd = para buildCmd'
+
+buildCmd' :: ScreenF (Screens, Cmd) -> Cmd
+buildCmd' (Primary n c)                     =         (buildCmd n) <> (buildCmd c) <> ["--primary"]
+buildCmd' (Secondary n c p (screens, cmds)) = cmds <> (buildCmd n) <> (buildCmd c) <> [positionArg p, name $ nextOutputName screens]
+buildCmd' (Disabled n c (_, cmds))          = cmds <> (buildCmd n) <> (buildCmd c) <> ["--off"]
+buildCmd' (Disconnected n (_, cmds))        = cmds <> (buildCmd n)                 <> ["--off"]
 
 positionArg :: Position -> T.Text
 positionArg LeftOf  = "--left-of"
@@ -107,117 +124,117 @@ positionArg Above   = "--above"
 positionArg Below   = "--below"
 positionArg SameAs  = "--same-as"
 
-class HasOutput a where
-  output :: a -> OutputName
+nextOutputName :: Screens -> OutputName
+nextOutputName = cata nextOutputName'
 
-instance HasOutput Output where
-  output = name
+nextOutputName' :: ScreenF OutputName -> OutputName
+nextOutputName' (Primary n _)      = n
+nextOutputName' (Secondary n _ _ _)  = n
+nextOutputName' (Disabled _ _ n)   = n
+nextOutputName' (Disconnected _ n) = n
 
-instance (HasOutput a) => HasOutput (ScreensF a b) where
-  output (Primary o) = output o
-  output (Secondary _ o _) = output o
+configWithNormalRotation :: Mode -> Config
+configWithNormalRotation = Config Normal
 
-primary :: a -> Outputs a
-primary = Fix . Primary
+data SC = 
+    Prim :: Screen
+  | Pre :: Screen -> Screen 
 
-secondary :: Position -> a -> Outputs a -> Outputs a
-secondary p x = Fix . (Secondary p x)
+instance SemiGroup SC where
+   (Prim s) <> (Prim _) = Prim s
+   (Pre f)  <> (Pre g)  = Pre $ g . f
+   (Prim s) <> (Pre f)  = Prim $ f s
+   (Pre f)  <> (Prim s) = Prim $ f s
 
-leftOf = secondary LeftOf
-rightOf = secondary RightOf
+main = undefined
 
-data OutputInfo = OutputInfo
-  { isPrimary  :: Bool
-  , outputInfo :: Output
-  } deriving (Show)
+-- parseXrandr = parseOnly $ parseOutputInfos
+-- 
+-- parseOutputInfos :: Parser SC
+-- parseOutputInfos = ignoreRestOfLine *> many parseOutputInfo
 
-instance HasOutput OutputInfo where
-  output = output . outputInfo
-
-parseXrandr = parseOnly $ parseOutputInfos
-
-parseOutputInfos :: Parser (Outputs Output)
-parseOutputInfos = ignoreRestOfLine *> many parseOutputInfo
-
-parseOutput :: Parser (Outputs Output) 
-parseOutput = 
-  (fmap . Output) 
-  <$> parseOutputName 
-  <*  skipSpace
-  <*> parseStatus
-
-parseOutputName :: Parser OutputName
-parseOutputName = takeTill (==' ')
-
-parseStatus :: Parser (ScreensF Status a)
-parseStatus = isDisconnected <|> isConnected
-
-isDisconnected :: Parser (ScreensF Status a)
+screenText :: Parser SC
+screenText = 
+      isPrimary
+  <|> isSeconday
+  <|> isDisabled
+  <|> isDisconnected
+  
+isDisconnected :: Parser SC
 isDisconnected = 
-  pure (False, Disconnected) 
+  ((Pre . Fix .) <$> Disconnected)
+  <$> outputNameText
+  <* skipSpace
   <* string "disconnected"
   <* ignoreRestOfLine
 
-isConnected :: Parser (Bool, Status)
-isConnected = 
-  pure (,)
-  <*  string "connected" 
+isPrimary :: Parser SC
+isPrimary = isConnected is
+
+isSeconday :: Parser 
+
+isConnected :: Parser Config -> Parser a -> Parser SC
+isConnected config primary =
+  ((Prim . Fix .) <$> Primary
+  <$> outputNameText
   <* skipSpace
-  <*> parsePrimary 
-  <*  skipSpace
-  <*  ignoreRestOfLine
-  <*> parseModes
+  <* string "connected" 
+  <* primary
+  <* ignoreRestOfLine
+  <*> config
 
-parsePrimary :: Parser Bool
-parsePrimary = flag $ string "primary"
+outputNameText :: Parser OutputName
+outputNameText = takeTill (==' ')
 
-parseModes :: Parser Status
-parseModes = 
-  (maybe Disabled (defaultConfig . fst) . find snd) 
-  <$> many parseModeAndEnabled
 
-defaultConfig :: Mode -> Status
-defaultConfig = Enabled . Config Normal
 
-parseModeAndEnabled :: Parser (Mode, Bool)
-parseModeAndEnabled = 
-      pure (,)
+modeWith :: Parser Bool -> Parser (Mode, Bool)
+modeWith p = 
+      ((,))
   <*  (many1 $ char ' ') 
-  <*> parseMode
+  <$> parseMode
   <*  skipSpace
   <*  take 5
-  <*> ((||) <$> (flag $ char '*') <*> (flag $ char '+'))
+  <*> p
   <*  ignoreRestOfLine
 
-parseMode :: Parser Mode
-parseMode = 
-  (,)
-  <$> parseNat 
+preferredMode
+
+preferredModeText :: Parser Bool
+preferredModeText = flag $ char '+'
+
+enabledModeText :: Parser Bool
+enabledModeText = flag $ char '*'
+
+modeText :: Parser Mode
+modeText = 
+  (Mode . (,))
+  <$> natText 
   <* char 'x' 
-  <*> parseNat
+  <*> natText
 
 flag :: Parser a -> Parser Bool
 flag p = pure True <* p <|> pure False
 
-parseNat :: Parser Natural
-parseNat = read <$> many1 digit
+natText :: Parser Natural
+natText = read <$> many1 digit
 
 ignoreRestOfLine :: Parser ()
 ignoreRestOfLine = takeTill isEndOfLine *> endOfLine
 
-main = readFile "out.txt" >>= either print buildAndRun . parseXrandr . T.pack
---   runXrandr []
---   >>= 
---     either print (buildAndRun >=> print)
---     . parseXrandr 
-
-buildAndRun = print . sortOn (status . outputInfo)
-
-runXrandr :: [T.Text] -> IO T.Text
-runXrandr = shelly . run "xrandr"
-
-asExternal :: (Foldable f, Functor f) => Position -> a -> f a -> Outputs a
-asExternal p = flip (asDir p) . primary 
-
-asDir :: (Foldable f) => Position -> f a -> Outputs a -> Outputs a
-asDir d = appEndo . foldMap (Endo . secondary d)
+-- main = readFile "out.txt" >>= either print buildAndRun . parseXrandr . T.pack
+-- --   runXrandr []
+-- --   >>= 
+-- --     either print (buildAndRun >=> print)
+-- --     . parseXrandr 
+-- 
+-- buildAndRun = print . sortOn (status . outputInfo)
+-- 
+-- runXrandr :: [T.Text] -> IO T.Text
+-- runXrandr = shelly . run "xrandr"
+-- 
+-- asExternal :: (Foldable f, Functor f) => Position -> a -> f a -> Outputs a
+-- asExternal p = flip (asDir p) . primary 
+-- 
+-- asDir :: (Foldable f) => Position -> f a -> Outputs a -> Outputs a
+-- asDir d = appEndo . foldMap (Endo . secondary d)
