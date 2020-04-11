5eb82e59656f621bc2ec5760ce06b57bab93de5f
theNerd247 <noah.harvey247 gm>
2020-04-11 14:25:17 -0700

    saving changes

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 # TODO:
 # [ ] fix hack where fish path puts user profile before system path
@@ -10,17 +10,35 @@
 
 let
   nixversion = "19.09";
-  nixplatform = if pkgs.system == "x86_64-darwin" then "nix-darwin" else "nixos";
+  nixplatform = if pkgs.stdenv.isDarwin then "nix-darwin" else "nixos";
 
   # use darwin config
   home-manager = builtins.fetchGit {
     url = "https://github.com/rycee/home-manager.git";
     ref = "release-${nixversion}";
   };
+
+  fetchChannel = revAndSHA: 
+    builtins.fetchTarball 
+      { inherit (revAndSHA) sha256; 
+        url="https://github.com/NixOS/nixpkgs/archive/${revAndSHA.rev}.tar.gz"; 
+      };
+
+  mkChannel = fetchChannel; #) { inherit config; };
+
+  pinnedPkgs = mkChannel
+    { rev = nixversion;
+      sha256 = "0mhqhq21y5vrr1f30qd2bvydv4bbbslvyzclhw0kdxmkgg3z4c92";
+    };
+
+  pkgs = import pinnedPkgs { inherit config; };
 in
 
 {
-  imports = [ (import "${home-manager}/nix-darwin") ];
+  imports = 
+    [ (import "${home-manager}/nix-darwin") 
+      (import ./noah.nix)
+    ];
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
@@ -28,19 +46,26 @@ in
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+  environment.darwinConfig = "$HOME/.nixpkgs/darwin-configuration.nix";
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+
+  #nix.package = pinnedPkgs;
+  nix.nixPath = "https://github.com/NixOS/nixpkgs/archive/${nixversion}.tar.gz";
 
   nix.extraOptions = ''
     build-users-group = nixbld
     system-features = benchmark big-parallel local nixos-test
   '';
-#     substituters = http://hydra.mv.awakenetworks.net:5000 https://cache.nixos.org/
-#     trusted-public-keys = hydra.mv.awakenetworks.net:ZfTJ89M1N5ndpH8wJBsMzmiZ20d29oHN8ql/X63NQC8= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
-#   '';
+
+  nix.binaryCaches = [
+    "http://hydra.mv.awakenetworks.net:5000"
+  ];
+
+  nix.binaryCachePublicKeys = [
+    "hydra.mv.awakenetworks.net:ZfTJ89M1N5ndpH8wJBsMzmiZ20d29oHN8ql/X63NQC8="
+  ];
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   # programs.bash.enable = true;
@@ -57,12 +82,8 @@ in
 
   # You should generally set this to the total number of logical cores in your system.
   # $ sysctl -n hw.ncpu
-  nix.maxJobs = if pkgs.system == "x86_64-darwin" then 10 else 1;
-  nix.buildCores = if pkgs.system == "x86_64-darwin" then 10 else 1;
+  nix.maxJobs = if pkgs.stdenv.isDarwin then 10 else 1;
+  nix.buildCores = if pkgs.stdenv.isDarwin then 10 else 1;
 
   home-manager.useUserPackages = false;
-
-  users.users.noah = import ./noah.nix { inherit pkgs; };
-
-  home-manager.users.noah = import ./home.nix { inherit nixversion; };
 }
