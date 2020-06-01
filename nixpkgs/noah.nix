b775e2dc84fb041df7d3bd2463ae0ad006bb1a40
theNerd247 <noah.harvey247 gm>
2020-06-01 13:11:21 -0700

    adds fixes for darwin

--- nixpkgs/noah.nix
+++ nixpkgs/noah.nix
@@ -1,11 +1,19 @@
 { config, pkgs, ...}:
 
-{ users.users.noah = 
+let
+  mkLinuxBuild = ops:
+    if pkgs.stdenv.isDarwin 
+    then ops 
+    else ops // 
+    { extraGroups = [ "wheel" ]; 
+      initialHashedPassword = "\$6\$4L//aDSh4X2T62c\$4fzzfUYmW.Uav8cY8kLA9uNtCunzoQEh8lU8tsqVbwPubqba6Sj4WNGRWSH7zTclMCddJCe.Z7aTvWrdAv0vn1";
+    };
+in
+
+{ users.users.noah = mkLinuxBuild
     { home  = if pkgs.stdenv.isDarwin then "/Users/noah" else "/home/noah";
       shell = pkgs.bash;
       createHome = true;
-      initialHashedPassword = "\$6\$4L//aDSh4X2T62c\$4fzzfUYmW.Uav8cY8kLA9uNtCunzoQEh8lU8tsqVbwPubqba6Sj4WNGRWSH7zTclMCddJCe.Z7aTvWrdAv0vn1";
-      extraGroups = [ "wheel" ];
     };
 
   home-manager.users.noah = import ./home.nix;
