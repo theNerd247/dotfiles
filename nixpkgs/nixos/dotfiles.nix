5d0e547548571e9a0f85ef2c199ae1410c064461
theNerd247 <noah.harvey247 gm>
2020-06-07 17:26:14 -0700

    adds ignoring generated iso files and saving copy of git repo in dotfiles

--- nixpkgs/nixos/dotfiles.nix
+++ nixpkgs/nixos/dotfiles.nix
@@ -1,10 +1,12 @@
 { stdenv }: 
 
 stdenv.mkDerivation
-{ srcs = ./../..;
+{ srcs = builtins.filterSource (path: type: baseNameOf path != "iso" ) ./../..;
   name = "dotfiles";
   installPhase = 
   '' mkdir -p $out/
     cp -r ./nixpkgs $out/nixpkgs
+    cp -r .git $out/.git
+    cp -r .gitignore $out/.gitignore
   '';
 }
