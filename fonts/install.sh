1784ddab1b1872e6d5abc69fcd5f5822f2f98a17
theNerd247 <noah.harvey247 gm>
2020-09-23 23:10:26 -0400

    removing uneeded install scripts

--- fonts/install.sh
+++
@@ -1,9 +0,0 @@
-# copied from: https://github.com/powerline/fonts
-# clone
-git clone https://github.com/powerline/fonts.git --depth=1
-# install
-cd fonts
-./install.sh
-# clean-up a bit
-cd ..
-rm -rf fonts
